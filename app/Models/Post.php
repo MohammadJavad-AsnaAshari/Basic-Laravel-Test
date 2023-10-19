8530c9ab1215cc4f2c799433cd7654b5b2642ec0
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-10-19 11:59:13 +0330

    add: post relationship with user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,8 +10,14 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = [
+        "user_id",
         "title",
         "description",
         "image"
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Models;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -22,5 +23,13 @@ class PostTest extends TestCase
         Post::create($post);
 
         $this->assertDatabaseHas("posts", $post);
+    }
+
+    public function test_post_relationship_with_user()
+    {
+        $post = Post::factory()->for(User::factory())->create();
+
+        $this->assertInstanceOf(User::class, $post->user);
+        $this->assertTrue(isset($post->user->id));
     }
 }
