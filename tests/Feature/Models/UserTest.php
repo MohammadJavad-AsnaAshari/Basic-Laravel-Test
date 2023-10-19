22cb82e85e55729e6372935a8a21f30901f895df
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-10-19 12:12:44 +0330

    add: user relationship with post

--- tests/Feature/Models/UserTest.php
+++ tests/Feature/Models/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -11,19 +12,29 @@ use function Symfony\Component\String\u;
 class UserTest extends TestCase
 {
     // Use the RefreshDatabase trait to reset the database after each test.
-//    use RefreshDatabase;
+    use RefreshDatabase;
 
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_insert_data()
+    public function test_insert_database()
     {
         $user = User::factory()->make()->toArray();
         $user["password"] = "password";
         User::create($user);
 
         $this->assertDatabaseHas("users", $user);
+    }
+
+    public function test_user_relationship_with_post()
+    {
+        $count = rand(0, 10);
+//        $user = User::factory()->has(Post::factory()->count($count))->create();
+        $user = User::factory()->hasPosts($count)->create();
+
+        $this->assertCount($count, $user->posts);
+        $this->assertInstanceOf(Post::class, $user->posts->first());
     }
 }
