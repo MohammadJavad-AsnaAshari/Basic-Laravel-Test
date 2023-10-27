b0b8c3bd5b5993f078b65729e9cb227661e95dbc
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-10-27 15:48:00 +0330

    test: Single View Render's form When User Logged In

---
+++ tests/Feature/Views/SingleViewTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Views;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SingleViewTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testSingleViewRenderWhenUserLoggedIn()
+    {
+        $user = User::factory()->create();
+        $post = Post::factory()->create();
+        $comments = [];
+
+        $view = (string) $this
+            ->actingAs($user)
+            ->view("single", compact(["post", "comments"]));
+
+        $dom = new \DOMDocument();
+        $dom->loadHTML($view);
+        $dom = new \DOMXPath($dom);
+
+        $action = route("single.comment", $post->id);
+
+        $this->assertCount(
+            1,
+            $dom->query("//form[@method='post'][@action='$action']/textarea[@name='text']")
+        );
+    }
+}
